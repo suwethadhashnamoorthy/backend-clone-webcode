5cb7c6779dd612ef60434e4b02b0f260f9a5c0a4
Suwetha <71821750+suwethadhashnamoorthy@users.noreply.github.com>
2023-03-20 15:12:37 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -21,3 +21,17 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+// const { Db } = require("mongoDB");
+// const mongoose = require("mongoose");
+
+// module.exports = () => {
+//   try {
+//     const uri = process.env.ATLAS_URI;
+//     mongoose.connect(uri, {});
+//     console.log("Connected to Database Successfully");
+//   } catch (error) {
+//     console.log(error);
+//     console.log("Connection Failed");
+//   }
+// };
